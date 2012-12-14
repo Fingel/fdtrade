31ab3bc1418349930100128dcee8a7f60560998a
Austin Riba <austin.rib@gmail.com>
2012-12-14 15:54:46 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fdtradeapp::Application.config.secret_token = '6641dde5b5bbe5e2fb1924945ce4a16a8b897fbcdae769a8331429ee91f1410fcb3274a9df9adf50d8aa3da1e0bb9f4c508356ffa4871afbfd293d871c2dc77e'
