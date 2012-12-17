ac92576b03b9472c13cd4173edbac75cbc9ab6e9
Austin Riba <austin.riba@gmail.com>
2012-12-16 19:06:08 -0800

    Add signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "StaticPages" do
   describe "Home Page" do
     it "Should have the content 'fdTrades'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('fdTrades')
     end
   end
