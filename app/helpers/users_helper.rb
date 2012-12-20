bd071021999a49c096a0888fa884a97b4f34e305
Austin Riba <austin.riba@gmail.com>
2012-12-19 22:58:51 -0800

    Added session support

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
 	#returns a gravatar for a users email address
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=http://toxiccode.com/misc/defaultgravatar.png"
 		image_tag(gravatar_url, alt: user.first + user.last, class: "gravatar")
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
   it { should respond_to(:ident) }
   it { should respond_to(:house_id) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate) }
 
   
@@ -121,6 +122,11 @@ end
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
 		it { should be_invalid }
+	end
+	
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 end
