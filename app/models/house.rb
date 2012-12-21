991df6a6915e8586af4769d2d3bc4e7c34ada3a4
Austin Riba <austin.riba@gmail.com>
2012-12-20 18:46:32 -0800

    Make some controllers admin only

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,5 @@ class House < ActiveRecord::Base
   belongs_to :department
   attr_accessible :address, :address2, :name, :number, :phone, :department_id, :battalion
   has_many :users
+  default_scope order: 'houses.number ASC'
 end
