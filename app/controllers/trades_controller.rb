03e6700fde6b21ee82772565a8a043cf80e00adb
Austin Riba <austin.riba@gmail.com>
2012-12-22 01:00:41 -0800

    add trade form to user profile page

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -6,8 +6,9 @@ class TradesController < ApplicationController
 		flash[:success] = "Trade created!"
 		redirect_to user_path(current_user)
 	else
-		flash[:error] = "Trade not saved."
+		flash[:error] = "Trade not saved. Please check for a valid date format (yyyy-mm-dd) an an option time in 24 hour format (hh:mm)"
 		redirect_to user_path(current_user)
+	end
   end
 
   def destroy
